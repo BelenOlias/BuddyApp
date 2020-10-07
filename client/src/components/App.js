e7c62abed602437b3928b1d16d731ba2f27dc341
Belén Olías <belolias@gmail.com>
2020-10-07 17:42:27 +0200

    Push martes y miércoles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,7 @@ import LogIn from './pages/logIn/LogIn'
 import Maps from './pages/maps/Maps'
 import Stadistics from './pages/stadistics/Stadistics'
 import Donation from './pages/donation/Donation'
+import DonationForm from './pages/donation/DonationForm'
 import DogList from './pages/dogList/DogList'
 import DogDetails from './pages/dogDetails/DogDetails'
 import Profile from "./pages/profile/Profile"
@@ -30,13 +31,15 @@ class App extends Component {
         <Navigation />
         <Switch>
           <Route path="/" exact render={() => <Index />} />
-          <Route path="/dogList" exact render={() => <DogList />} />
-          <Route path="/dogList/:dog_id"  render={props => <DogDetails {...props} />} />
+          <Route path="/dogList" exact render={props => <DogList {...props}/>} />
+          <Route path="/dogList/:dog_id" render={props => <DogDetails {...props} />} />
+          <Route path="/donation" exact render={() => <Donation />} />
+          <Route path="/donation/donationForm" render={() => <DonationForm />} />
+          <Route path = "/map"  render= {() => <Maps/>} />
+
           {/* <Route path ="/signup"  render={() => <SignUp/>} />
           <Route path = "/login"  render= {() => <LogIn/>} />
-          <Route path = "/map"  render= {() => <Maps/>} />
           <Route path = "/stadistics"  render= {() => <Stadistics/>} />
-          <Route path = "/donation"  render = {() => <Donation/>} />
           <Route path = "/profile"  render = {() => <Profile/>} /> */}
 
         </Switch>
